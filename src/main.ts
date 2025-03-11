4bfce08c93c7a1b54b2e7f089903323c180efd80
Jas <50244053+JasDevelops@users.noreply.github.com>
2025-03-11 15:20:18 +0100

    Re-Run TypeDoc

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+/**
+ * @summary Bootstraps the Angular application.
+ * Initializes the app by setting up the root component (`AppComponent`)
+ * and applying configuration settings from `appConfig`.
+ */
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
